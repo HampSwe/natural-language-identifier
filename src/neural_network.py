4f1a377217841230a4eff80da7843d53c7cfc131
Hampus Serneke <hampus.serneke@gmail.com>
2022-09-18 13:01:49 +0200

    Improve type annotation in neural_network.py

--- src/neural_network.py
+++ src/neural_network.py
@@ -15,7 +15,7 @@ import tensorflow as tf
 from tensorflow import keras as k
 import numpy as np
 
-def layerize(word_list: list, use_bools: bool = True) -> np.ndarray:
+def layerize(word_list: list[str], use_bools: bool = True) -> np.ndarray:
     """Function that layerizes a word in order for the program to be able to feed it to the trained model"""
 
     char_dict = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9, "k":10, "l":11, "m":12, "n":13, "o":14, "p":15, "q":16, "r":17, "s":18, "t":19, "u":20, "v":21, "w":22, "x":23, "y":24, "z":25, "å":26, "ä":27, "ö":28, "-":29, "é":30}
